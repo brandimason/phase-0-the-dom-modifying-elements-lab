0cb4523fa06ac3f13902a43149b425e2cf9be776
Brandi L Mason <masonbrandi66@gmail.com>
2023-06-22 19:49:12 -0600

    did it again

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // Write your code here!
-document.querySelector("main").remove();
+// document.querySelector("main").remove();
 
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
-newHeader.textContent ="Brandi is the champion";
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+// newHeader.textContent ="Brandi is the champion";
 
-document.body.append(newHeader);
+// document.body.append(newHeader);
+
+
+
+
+const removeMainElement = document.querySelector('main');
+removeMainElement.remove();
+
+const newHeader = document.createElement('h1'); // create a new h1
+// console.log(newHeader)
+//giving to element an id
+newHeader.id = 'victory'
+newHeader.textContent = "Brandi is the champion"
+// now i need to append it to the DOM
+const getBodyToAppend = document.querySelector('body'); // needed to grab where i need to append to
+getBodyToAppend.append(newHeader); // appended new element to the body
+
+//
+
